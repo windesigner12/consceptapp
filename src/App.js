01c6ecb3f11b17463607d669f15dee0905e20393
winshelo <winshelo@hotmail.com>
2023-05-02 15:01:56 -0400

    First push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +14,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+
+      
+      </header> */}
+      <h1>Hello World1</h1>
+
     </div>
   );
 }
